ee49c9a74cc130d083802e4e3e61b36717942109
Zyrakia <mailzyrakia@gmail.com>
2021-12-18 22:08:07 -0800

    test: added event connection test

--- src/__tests__/Make.spec.ts
+++ src/__tests__/Make.spec.ts
@@ -5,20 +5,40 @@ export = () => {
 	it('should be able to create an instance', () => {
 		const instance = Make('Part');
 		expect(typeIs(instance, 'Instance')).to.equal(true);
+		instance.Destroy();
 	});
 
 	it('should be able to parent the instance', () => {
 		const instance = Make('Part', { Parent: Workspace });
 		expect(instance.Parent).to.equal(Workspace);
+		instance.Destroy();
 	});
 
 	it('should be able to add a single child', () => {
 		const instance = Make('Part', { Children: Make('Part') });
 		expect(instance.GetChildren().size()).to.equal(1);
+		instance.Destroy();
 	});
 
 	it('should be able to add multiple children', () => {
 		const instance = Make('Part', { Children: [Make('Part'), Make('Part')] });
 		expect(instance.GetChildren().size()).to.equal(2);
+		instance.Destroy();
+	});
+
+	it('should connect events properly', () => {
+		let deparented = false;
+
+		const instance = Make('Part', {
+			Parent: Workspace,
+			AncestryChanged: (_, parent) => {
+				if (!parent) return;
+				deparented = true;
+			},
+		});
+
+		instance.Parent = undefined;
+		expect(deparented).to.equal(true);
+		instance.Destroy();
 	});
 };
